a18396fc5cee053465ef46a8e0aa8b751df82711
Sebastian <storo3@outlook.com>
2023-11-05 16:22:09 -0500

    update app

--- src/app.rs
+++ src/app.rs
@@ -251,25 +251,6 @@ impl eframe::App for GenApp {
                 // file.write_all(yaml.as_bytes()).unwrap();
                 // self.has_generated = true;
             }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
